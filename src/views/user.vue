81e3a7628b242041c060e053fb0a9fc845327b3d
siriue <18340826486@163.com>
2019-05-08 01:09:25 +0800

    用户管理完成

--- src/views/user.vue
+++ src/views/user.vue
@@ -18,16 +18,31 @@
                     </template>
                 </el-table-column>
                 <el-table-column prop="tel" label="电话号码"></el-table-column>
-                <el-table-column prop="carInfo" label="拥有车辆"></el-table-column>
+                <el-table-column label="拥有车辆">
+                    <template slot-scope="scope">
+                        <div v-for="(car) in scope.row.carList">
+                           <span>{{car.model}}</span>
+                            <span><el-button type="danger" size="mini" @click="deleteCar(scope.row,car)">删除</el-button></span>
+                        </div>
+                    </template>
+                </el-table-column>
                 <el-table-column prop="comment" label="备注"></el-table-column>
-                <el-table-column label="操作">
+                <el-table-column label="操作" min-width="230">
                     <template slot-scope="scope">
                         <el-button type="primary" size="mini" @click="handleModify(scope.row)">修改</el-button>
                         <el-button type="danger" size="mini" @click="deleteUser(scope.row)">删除</el-button>
+                        <el-button type="primary" size="mini" @click="handleClickAddCar(scope.row)">添加车辆</el-button>
                     </template>
                 </el-table-column>
             </el-table>
         </div>
+        <el-dialog :visible="addCarDialog" width="50%" title="添加车辆" @close="closeDialog">
+            <el-radio v-model="selectedRadio" :label="car.id" v-for="(car) in showCarList">{{car.model}}</el-radio>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="addCar">添加</el-button>
+                <el-button type="primary" @click="closeDialog">关闭</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -49,6 +64,11 @@
         width: 100%;
         .user-table {
             width: 100%;
+            table {
+                .el-button__danger {
+                    padding : 5px 5px;
+                }
+            }
         }
     }
 </style>
@@ -65,18 +85,23 @@
         data() {
             return {
                 userList: [],
+                addCarDialog: false,
+                currentUserId: null,
+                carList: [],
+                showCarList: [],
+                selectedRadio: null,
             }
         },
 
         methods: {
             formatResponse(res) {
                 res.forEach(it => {
-                    let carInfo = "";
+                    let userCarIdList = [];
                     it.carList.forEach(car => {
-                        carInfo = car.model + " ";
-                    })
-                    it.carInfo = carInfo;
-                })
+                        userCarIdList.push(car.id);
+                    });
+                    it.userCarIdList = userCarIdList;
+                });
                 this.userList = res;
             },
 
@@ -97,7 +122,7 @@
                     this.$message({
                         type : "success",
                         message: "删除成功"
-                    })
+                    });
                     console.log(res);
                     this.getUserList();
                 }).catch(e => {
@@ -112,6 +137,58 @@
             handleModify(row) {
                 this.$store.commit("dataTransfer", row);
                 this.$router.push("userAdd");
+            },
+
+            handleClickAddCar(row) {
+                this.axios.post("/api/user/queryAllCar").then(res => {
+                    console.log(res);
+                    this.carList = res.data;
+                    this.showCarList = this.carList.filter(it => {
+                        return row.userCarIdList.indexOf(it.id) === -1;
+                    })
+                    console.log(this.showCarList);
+                });
+                this.currentUserId = row.userId;
+                this.addCarDialog = true;
+            },
+
+            closeDialog() {
+                this.currentUserId = null;
+                this.carList = [];
+                this.showCarList = [];
+                this.selectedRadio = null;
+                this.addCarDialog = false;
+            },
+
+            addCar() {
+                let payload = {
+                    userId: this.currentUserId,
+                    carId: this.selectedRadio,
+                };
+                this.axios.post("/api/user/addCarByUserId", payload).then(res => {
+                    console.log(res);
+                    this.$message({
+                        type : "success",
+                        message: "添加成功",
+                    });
+                    this.getUserList();
+                    this.closeDialog();
+                });
+            },
+
+            deleteCar(row, car) {
+                let payload = {
+                    userId: row.userId,
+                    carId: car.id,
+                }
+                this.axios.post("/api/user/deleteRelation", payload).then(res => {
+                    console.log(res);
+                    this.$message({
+                        type : "success",
+                        message: "删除成功",
+                    });
+                    this.getUserList();
+                });
             }
         }
     }
